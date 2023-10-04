06001692e0effe080cbd3972c3dc173d71d88c8d
Ririn Julianansa <ririn.210170242@mhs.unimal.ac.id>
2023-10-04 09:21:30 +0700

    function

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ window.onload = () => {
     const button = document.querySelector('#btn');
     button.addEventListener('click', calculateBmi)
 }
+
+function calculateBmi() {
+    const height = document.querySelector('#height').value;
+    const weight = document.querySelector('#weight').value;
+    const result = document.querySelector('#result');
+
+    if (!height || isNaN(height) || height < 0) {
+        result.innerText = "Please provide a valid height";
+        return;
+    } else if (!weight || isNaN(weight) || weight < 0) {
+        result.innerText = "Please provide a valid weight";
+        return;
+    }
+}
