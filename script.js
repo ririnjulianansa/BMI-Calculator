15ae5ee6d8e95519e9c7ddf6e92f13b0a1d89c2d
Ririn Julianansa <ririn.210170242@mhs.unimal.ac.id>
2023-10-04 09:23:46 +0700

    rumus

--- script.js
+++ script.js
@@ -15,4 +15,21 @@ function calculateBmi() {
         result.innerText = "Please provide a valid weight";
         return;
     }
+
+    const bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
+
+    if (bmi < 18.5) {
+        result.innerText = `Under Weight: ${bmi}`;
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        result.innerText = `Normal Weight: ${bmi}`;
+    } else if (bmi >= 25 && bmi < 29.9) {
+        result.innerText = `Overweight: ${bmi}`;
+    } else if (bmi >= 30 && bmi < 34.9) {
+        result.innerText = `Obesity: ${bmi}`;
+    } else if (bmi >= 35.5 && bmi < 39.9) {
+        result.innerText = `Obesity (Class II): ${bmi}`;
+    } else {
+        result.innerText = `Extreme Obesity: ${bmi}`;
+    }
+
 }
